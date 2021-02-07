0531c96b3ace230dd13ccdc0104f37482e43d9a4
IlyaKIA <IlyaKIA03@gmail.com>
2021-02-07 17:38:01 +0300

    add button and check empty line

--- Chat/Window.java
+++ Chat/Window.java
@@ -12,17 +12,42 @@ public class Window extends JFrame {
         JTextArea historyText = new JTextArea();
         JButton insertTextBtn = new JButton();
         historyText.setFocusable(false);
+        insertTextBtn.setText(">");
+        Container insertTextCont = new Container();
+        insertTextCont.setLayout( new BorderLayout());
+        insertTextCont.add(textInput, BorderLayout.CENTER);
+        insertTextCont.add(insertTextBtn, BorderLayout.EAST);
+
         setLayout(new BorderLayout());
-        add(textInput, BorderLayout.PAGE_END);
+        add(insertTextCont, BorderLayout.PAGE_END);
         add(historyText, BorderLayout.CENTER);
 
         textInput.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                historyText.setText(historyText.getText() + textInput.getText() + "\n");
-                textInput.setText("");
+                insertTextMetod(historyText, textInput);
             }
         });
+        insertTextBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertTextMetod(historyText, textInput);
+            }
+        });
+
         setVisible(true);
     }
+
+    private void insertTextMetod(JTextArea historyText, JTextField textInput) {
+        String errText = "Введите текст";
+        if (textInput.getText().equals("")) {
+            textInput.setText(errText);
+            textInput.select(0,errText.length());
+        } else {
+            historyText.setText(historyText.getText() + textInput.getText() + "\n");
+            textInput.setText("");
+        }
+    }
+
+
 }
